4a30c9c73d17729ad49984d1be0ef5bc67e5a198
milanPatel001 <mp28238@gmail.com>
2023-07-28 20:00:35 -0400

    client fix

--- client.js
+++ client.js
@@ -1,12 +1,14 @@
 const socketIOClient = require("socket.io-client");
 
-const SERVER_URL = "http://192.168.1.100:3000"; // Replace with the server's IP address and port
+const SERVER_URL = "http://localhost:3000"; // Replace with the server's IP address and port
 
 const socket = socketIOClient(SERVER_URL);
 
 socket.on("connect", () => {
   console.log("Connected to server!");
 });
+
+//socketClient.emit("new message", { hello: "hello" });
 
 socket.on("new message", (data) => {
   console.log("Received message from server:", data);
@@ -15,3 +17,9 @@ socket.on("new message", (data) => {
 socket.on("disconnect", () => {
   console.log("Connection to server closed");
 });
+
+/*
+while (true) {
+  console.log("Enter something:");
+}
+*/
