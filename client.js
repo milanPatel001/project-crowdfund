cf34d1dd476e0dd7ecbaa1e90cfc4ec18fce6392
milanPatel001 <mp28238@gmail.com>
2023-07-30 13:08:24 -0400

    fixed everything

--- client.js
+++ client.js
@@ -1,25 +1,49 @@
 const socketIOClient = require("socket.io-client");
+const prompt = require("prompt-async");
 
-const SERVER_URL = "http://localhost:3000"; // Replace with the server's IP address and port
+const SERVER_URL = "http://98.113.25.59:65535"; // Replace with the server's IP address and port
 
 const socket = socketIOClient(SERVER_URL);
 
-socket.on("connect", () => {
+socket.on("connect", async () => {
   console.log("Connected to server!");
+
+  //await startInteractiveLoop();
 });
 
-//socketClient.emit("new message", { hello: "hello" });
+socket.emit("new message", { hello: "hello" });
 
 socket.on("new message", (data) => {
   console.log("Received message from server:", data);
+});
+
+socket.on("curr value", (value) => {
+  console.log("Current total value", value);
 });
 
 socket.on("disconnect", () => {
   console.log("Connection to server closed");
 });
 
+let opt = "";
+
 /*
-while (true) {
-  console.log("Enter something:");
+async function startInteractiveLoop() {
+  while (opt !== "q") {
+    prompt.start();
+    const { v } = await prompt.get(["v"]);
+    opt = v;
+    console.log(v);
+
+    if (opt === "1") {
+      prompt.start();
+      const { bid } = await prompt.get(["bid"]);
+      Number(bid);
+
+      socket.emit("bid", bid);
+    } else if (opt === "q") {
+      socket.emit("quit");
+    }
+  }
 }
 */
