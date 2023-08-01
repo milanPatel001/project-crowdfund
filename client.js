e825c4809afd4d47f2af6df173186f2e95b6edef
milanPatel001 <mp28238@gmail.com>
2023-07-31 20:03:14 -0400

    improved output quality

--- client.js
+++ client.js
@@ -2,7 +2,7 @@ const socketIOClient = require("socket.io-client");
 const { setTimeout } = require("timers/promises");
 const readline = require("readline");
 
-const readlineInterface = readline.createInterface({
+const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
@@ -12,9 +12,9 @@ const SERVER_URL_LOCAL = "http://localhost:3000";
 
 const socket = socketIOClient(SERVER_URL_LOCAL);
 
-function ask(questionText) {
+function ask(question) {
   return new Promise((resolve, reject) => {
-    readlineInterface.question(questionText, (input) => resolve(input));
+    rl.question(question, (input) => resolve(input));
   });
 }
 
@@ -38,24 +38,58 @@ socket.on("disconnect", () => {
   console.log("Connection to server closed");
 });
 
-let option = "";
+socket.on("donation amount value", ({ value }) => {
+  console.log("Current amount: ", value);
+});
+
+let option = "-";
 
 async function startInteractiveLoop() {
   while (option !== "q") {
-    await setTimeout(1000);
+    await setTimeout(1000); //timeout for ordered display
 
-    console.log("Select an option");
-    console.log("1. Donate\nq. Disconnect from the server.");
+    console.log("\nSelect an option");
+    console.log(
+      "1. Donate\n2. See the Donation amount\nq. Disconnect from the server."
+    );
 
     option = await ask("\nEnter Option: ");
     if (option === "1") {
-      let donation = await ask("Enter a donation amount: ");
-      donation = Number(donation);
-
-      socket.emit("donate", donation);
+      await handleDonation();
+    } else if (option === "2") {
+      await getDonationAmount();
     } else if (option === "q") {
       socket.emit("quit");
       break;
     }
+  }
+}
+
+async function handleDonation() {
+  console.log("Choose a crowdfund to donate to:");
+  console.log(
+    "1. ABC Corp.\n2. Cancer fund\n3. Saving Environment Fund\n4. Scam Fund\n5. Education Fund"
+  );
+
+  let option = await ask("Enter the option here: ");
+
+  if (Number(option) >= 1 && Number(option) <= 5) {
+    let donation = await ask("Enter a donation amount: ");
+    donation = Number(donation);
+
+    socket.emit("donate", { amount: donation, index: Number(option) - 1 });
+  }
+}
+
+async function getDonationAmount() {
+  console.log("\nWhich crowdfund's amount do you want to see?");
+  console.log(
+    "1. ABC Corp.\n2. Cancer fund\n3. Saving Environment Fund\n4. Scam Fund\n5. Education Fund"
+  );
+
+  let option = await ask("Enter the option here: ");
+
+  if (Number(option) >= 1 && Number(option) <= 5) {
+    socket.emit("donation amount request", { index: Number(option) - 1 });
   }
 }
