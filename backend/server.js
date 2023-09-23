d837c44098bf72a11d5d14341c48694d52bee997
milanPatel001 <mp28238@gmail.com>
2023-09-23 19:46:59 -0400

    changed last line

--- backend/server.js
+++ backend/server.js
@@ -1098,6 +1098,6 @@ ioserver.on("connect_failed", () =>
 );
 
 // starts the server that listen to a specified port
-app.listen(port, "0.0.0.0", () => {
+server.listen(port, "0.0.0.0", () => {
   console.log(`Server running on port ${port}`);
 });
